36e905eb85c1bb1094dd158fbbe669e1b5e6746a
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-04-29 10:00:21 -0300

    first commit

---
+++ src/test/java/br/com/maxsueleinstein/cuponomia/CuponomiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maxsueleinstein.cuponomia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuponomiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
